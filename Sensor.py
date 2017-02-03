c1d771da2d4b38d014241d5eda53b63fe42a4572
quanglys <quanglys@gmail.com>
2017-02-03 17:30:14 +0700

    add try-except

--- Sensor.py
+++ Sensor.py
@@ -19,22 +19,30 @@ def readConfig():
 
 #Number    MyTemperature  "Temperature [%.1f °C]"         {mqtt="<[mos:/temp:state:default], >[mos:/out:state:*:default]"}
 def writeItemFile():
-    f = open(pathConfig + '/items/demo.items', 'w')
-    for i in range(0, numberSensor):
-        f.write('Number Sensor' + str(i) + ' "Value [%.1f]" {mqtt="<[mqttIn:/in' + str(i) + ':state:default], >[mqttOut:/out' + str(i) + ':state:*:default]"}' + '\n')
-    f.close()
+    try:
+        f = open(pathConfig + '/items/demo.items', 'w')
+        for i in range(0, numberSensor):
+            f.write('Number Sensor' + str(i) + ' "Value [%.1f]" {mqtt="<[mqttIn:/in' + str(i) + ':state:default], >[mqttOut:/out' + str(i) + ':state:*:default]"}' + '\n')
+    except IOError:
+        pass
+    else:
+        f.close()
 
 # sitemap demo label="Demo House"
 # {
 # 	Text item=MyTemperature
 # }
 def writeSitemapFile():
-    f = open(pathConfig + '/sitemaps/demo.sitemap', 'w')
-    f.write('sitemap demo label="Demo House"\n{' + '\n')
-    for i in range(0, numberSensor):
-        f.write('Text item=Sensor' + str(i) + '\n')
-    f.writelines('}')
-    f.close()
+    try:
+        f = open(pathConfig + '/sitemaps/demo.sitemap', 'w')
+        f.write('sitemap demo label="Demo House"\n{' + '\n')
+        for i in range(0, numberSensor):
+            f.write('Text item=Sensor' + str(i) + '\n')
+        f.writelines('}')
+    except IOError:
+        pass
+    else:
+        f.close()
 
 def sendData(i):
     while bStop:
