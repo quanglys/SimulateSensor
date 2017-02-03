dc100ff35f3b0822b047290eb4b3a5058e5350ec
quanglys <quanglys@gmail.com>
2017-02-03 18:50:14 +0700

    change lib

--- Sensor.py
+++ Sensor.py
@@ -2,7 +2,7 @@
 import paho.mqtt.client as mqtt
 import random
 import time
-import thread
+import _thread
 
 MILISECOND = 0.001
 mqttc = mqtt.Client('python_pub')
@@ -55,7 +55,7 @@ writeSitemapFile()
 try:
     # sendData(0)
     for i in range(0, numberSensor):
-        thread.start_new_thread(sendData, (i,))
+        _thread.start_new_thread(sendData, (i,))
 except:
     print ('Can not create thread\n')
 
