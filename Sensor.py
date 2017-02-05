275ff426e1f0bba3e78d57bbc776264cf750abf6
quanglys <quanglys@gmail.com>
2017-02-05 22:13:31 +0700

    fix

--- Sensor.py
+++ Sensor.py
@@ -5,7 +5,7 @@ import time
 import _thread
 
 def readConfig():
-    f = open('config/config.cfg', 'r')
+    f = open('/SimulateSensor/config/config.cfg', 'r')
     global ipMos
     ipMos = f.readline().replace('\n','')
     global numberSensor
@@ -15,7 +15,7 @@ def readConfig():
 #Number    MyTemperature  "Temperature [%.1f °C]"         {mqtt="<[mos:/temp:state:default], >[mos:/out:state:*:default]"}
 def writeItemFile():
     try:
-        f = open('./openhab/items/demo.items', 'w')
+        f = open('/openhab/items/demo.items', 'w')
         for i in range(0, numberSensor):
             f.write('Number Sensor' + str(i) + ' "Value [%.1f]" {mqtt="<[mqttIn:/in' + str(i) + ':state:default], >[mqttOut:/out' + str(i) + ':state:*:default]"}' + '\n')
     except IOError:
@@ -29,7 +29,7 @@ def writeItemFile():
 # }
 def writeSitemapFile():
     try:
-        f = open('./openhab/sitemaps/demo.sitemap', 'w')
+        f = open('/openhab/sitemaps/demo.sitemap', 'w')
         f.write('sitemap demo label="Demo House"\n{' + '\n')
         for i in range(0, numberSensor):
             f.write('Text item=Sensor' + str(i) + '\n')
