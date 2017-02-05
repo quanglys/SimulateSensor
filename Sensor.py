f031f8484d84ceda27f64f7deb94f4fbd5430d30
quanglys <quanglys@gmail.com>
2017-02-05 21:59:47 +0700

    fix

--- Sensor.py
+++ Sensor.py
@@ -2,17 +2,12 @@
 import paho.mqtt.client as mqtt
 import random
 import time
-import _thread
-
-MILISECOND = 0.001
-mqttc = mqtt.Client('python_pub')
-mqttc.connect('192.168.1.40', 1883)
-bStop = 1
+import thread
 
 def readConfig():
-    f = open('./SimulateSensor/config/config.cfg', 'r')
-    global pathConfig
-    pathConfig = f.readline().replace('\n','')
+    f = open('config/config.cfg', 'r')
+    global ipMos
+    ipMos = f.readline().replace('\n','')
     global numberSensor
     numberSensor = int(f.readline())
     f.close()
@@ -20,7 +15,7 @@ def readConfig():
 #Number    MyTemperature  "Temperature [%.1f °C]"         {mqtt="<[mos:/temp:state:default], >[mos:/out:state:*:default]"}
 def writeItemFile():
     try:
-        f = open(pathConfig + '/items/demo.items', 'w')
+        f = open('./openhab/items/demo.items', 'w')
         for i in range(0, numberSensor):
             f.write('Number Sensor' + str(i) + ' "Value [%.1f]" {mqtt="<[mqttIn:/in' + str(i) + ':state:default], >[mqttOut:/out' + str(i) + ':state:*:default]"}' + '\n')
     except IOError:
@@ -34,7 +29,7 @@ def writeItemFile():
 # }
 def writeSitemapFile():
     try:
-        f = open(pathConfig + '/sitemaps/demo.sitemap', 'w')
+        f = open('./openhab/sitemaps/demo.sitemap', 'w')
         f.write('sitemap demo label="Demo House"\n{' + '\n')
         for i in range(0, numberSensor):
             f.write('Text item=Sensor' + str(i) + '\n')
@@ -49,17 +44,25 @@ def sendData(i):
         mqttc.publish('/in' + str(i), random.randint(-100, 100))
         time.sleep(random.randint(100,1000) * MILISECOND)
 
+
+MILISECOND = 0.001
 readConfig()
+mqttc = mqtt.Client('python_pub')
+mqttc.connect(ipMos, 1883)
+bStop = 1
 writeItemFile()
 writeSitemapFile()
 try:
     # sendData(0)
     for i in range(0, numberSensor):
-        _thread.start_new_thread(sendData, (i,))
+        thread.start_new_thread(sendData, (i,))
 except:
     print ('Can not create thread\n')
 
 while bStop:
-    time.sleep(100)
+    c = raw_input('Press key "s" to stop\n')
+    c = str.upper(c)
+    if (c == 'S'):
+        bStop = 0
 
 
